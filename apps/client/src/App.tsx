6734809f849d50588f2f5e375889d7f7571fba11
Abhilash Pandey <pandeyabhilash87@gmail.com>
2026-06-14 19:30:19 +0530

    setup shadcn ui in frontend

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import './App.css';
+import {Button} from "@/components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="font-bold p-4 text-3xl">Hello, Vite + React!</div>
+      <Button>Click me</Button>
     </>
   )
 }
